4b9329d846566fe0709d549e526c9b9de2f6d6c0
Ramesh Kurmi <rameshkurmi060@gmail.com>
2026-06-11 14:59:52 +0900

    footer updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,8 @@
 </main>
 <footer class="main-footer">
-    <p>&copy; 2026 Dong-Eui University - Department of Intelligence Computing. Course: Web Programming 1</p>
+    <p>&copy; 2026 Dong-Eui University - Department of Intelligence Computing. OIMS Project
+        
+    </p>
 </footer>
 <script src="../assets/js/clipboard.js"></script>
 </body>
